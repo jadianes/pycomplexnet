4230e4c7293fb1be05d6d75405b3e23d6ae8f000
Jose A Dianes <jadianes@gmail.com>
2021-06-10 11:15:50 +0100

    degree distributions plus example notebook

--- pycomplexnet/network.py
+++ pycomplexnet/network.py
@@ -1,9 +1,14 @@
 
+import numpy as np
+
 class Network:
-    """A Complex Network representation"""
+    """A directed graph representation of a Network"""
  
     def __init__(self, adjacency_matrix):
         self.adjacency_matrix = adjacency_matrix
+
+    def toarray(self):
+        return self.adjacency_matrix.toarray()
 
     def get_in_degree(self, node_i):
         return self.adjacency_matrix.getcol(node_i).sum()
@@ -17,5 +22,35 @@ class Network:
     def get_out_degrees(self):
         return self.adjacency_matrix.sum(1).flatten()
 
-    def toarray(self):
-        return self.adjacency_matrix.toarray()
+    def get_joint_degree_dist(self, k_in, k_out):
+        return (
+            (1.0/self.adjacency_matrix.get_shape()[0])
+            *(
+                float(np.sum(
+                    np.multiply(
+                        self.get_in_degrees()==k_in,
+                        self.get_out_degrees()==k_out
+                    )
+                ))
+            )
+        )
+
+    def get_in_degree_dist(self, k_in):
+        return (
+            (1.0/self.adjacency_matrix.get_shape()[0])
+            *(
+                float(np.sum(
+                    self.get_in_degrees()==k_in
+                ))
+            )
+        )
+    
+    def get_out_degree_dist(self, k_out):
+        return (
+            (1.0/self.adjacency_matrix.get_shape()[0])
+            *(
+                float(np.sum(
+                    self.get_out_degrees()==k_out
+                ))
+            )
+        )
