5c6da8781e1e2acb1e6d90ec87e9067e5a18107b
Jose A Dianes <jadianes@gmail.com>
2021-06-27 08:18:10 +0100

    generator for Zachary Karate Club network

--- pycomplexnet/generators.py
+++ pycomplexnet/generators.py
@@ -1,6 +1,10 @@
+from os import sep
 from .network import Network
 from scipy.sparse import coo_matrix, rand
 import numpy as np
+import pandas as pd
+import pkgutil
+import io
 
 def disconnected_network(n_nodes):
     adjacency_matrix = coo_matrix((n_nodes,n_nodes))
@@ -17,3 +21,10 @@ def star_network(n_nodes, center_node):
     a[center_node,center_node] = 0.0
     adjacency_matrix = coo_matrix(a)
     return Network(adjacency_matrix)
+
+def zachary_karate_club_77():
+    # read data from resources to create data frame of links
+    edges_file = pkgutil.get_data(__name__, "data/zkcc-77/karate_edges_77.txt").decode()
+    edges_df = pd.read_csv(io.StringIO(edges_file), sep='\s', header=None, names=['from','to'])
+    
+    return Network.from_pandas(edges_df)
